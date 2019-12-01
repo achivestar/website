d0418e808b0f9d13ddd8fe04ae266966955b2f7b
sds <achivestar>
2019-12-01 23:27:32 +0900

    php website

--- memo/memo.php
+++ memo/memo.php
@@ -17,7 +17,24 @@ session_start();
             .lmenu {
                 display: none
             }
+
+            .memobtn {
+                width:100%;
+            }
+
+
         }
+
+        @media screen and (min-width: 1200px) {
+            .memobtn {
+                width:100%;
+                height:160px;
+            }
+        }
+
+
+
+
     </style>
 
 </head>
@@ -27,6 +44,9 @@ session_start();
     <?php
     include "../lib/top_login_sub.php";
     include "../lib/top_menu_sub.php";
+    include_once ("memoDao.php");
+    $dao = new memoDao();
+    $numMsgs = $dao->selectMemo();
     ?>
     <div class="row">
         <div class="col-sm-4 lmenu">
@@ -41,17 +61,41 @@ session_start();
                 <a href="#" class="list-group-item list-group-item-action">설문조사</a>
             </div>
         </div>
-        <div class="col-sm-8 col-12">
+        <div class="col-sm-8 col-12 container">
             <br>
             <h2>메모장</h2>
             <br><br>
+            <div><span><?php if($_SESSION["usernick"]){?>▷<?=$_SESSION["usernick"]?><?php } ?></span></div>
             <form name="memo_form" method="post" action="insert.php">
-                <span>▷<?=$_SESSION["usernick"]?></span>
-                <textarea rows="6" cols="95" name="content"></textarea>
-                <input type="submit" value="메모하기" />
+                <div class="row">
+                    <div class="col-sm-10">
+                       <textarea rows="6" class="form-control" name="content"></textarea>
+                        <br>
+                    </div>
+
+                    <div class="col-sm-2 col-12">
+                      <input type="submit" class="btn btn-info memobtn" value="메모하기" />
+                    </div>
+
+                </div>
+                <br>
             </form>
-        </div>
-    </div>
+
+                <?php foreach ($numMsgs as $row) : ?>
+                  <div style="background: #D4F4FA;border-radius:5px">
+                      <p  style="text-indent: 10px"><?=$row["num"]?>&nbsp;<?=$row["nick"]?>&nbsp;&nbsp;&nbsp;<?=$row["regist_day"]?>
+                        &nbsp;<?php if($_SESSION["userid"]=="admin" || $_SESSION["userid"] == $row["id"]){?>
+                            <a href="delete.php?num=<?=$row["num"]?>" class="btn btn-link" role="button">삭제</a>
+                             <?php } ?></p>
+                        <hr>
+                        <p style="text-indent: 10px"><?=$row["content"]?></p>
+                  </div>
+                    <br><br>
+                <?php endforeach; ?>
+
+      </div>
+
+    </div><!-- row end -->
 
 
 </div>
